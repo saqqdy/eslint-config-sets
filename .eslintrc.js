235e71073c2c56b8e5477d9b30b3c77d56631701
saqqdy <saqqdy@qq.com>
2020-10-28 09:36:29 +0800

    1.0.0

---
+++ .eslintrc.js
@@ -0,0 +1,4 @@
+const config = require('./config/simple.json');
+module.exports = {
+	...config,
+};
---
+++ index.js
@@ -0,0 +1,7 @@
+const simple = require('./config/simple.json');
+const vue = require('./config/vue.json');
+const vue3 = require('./config/vue3.json');
+
+module.exports.simple = simple;
+module.exports.vue = vue;
+module.exports.vue3 = vue3;
